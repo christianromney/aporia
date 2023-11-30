0ee7f8d275aa3670f5cef46b3bb9f0b8f3d45b8d
Christian Romney <christian.romney@cognitect.com>
2023-11-30 01:25:43 -0500

    Randomize the voices

--- src/aporia/cli.clj
+++ src/aporia/cli.clj
@@ -94,7 +94,7 @@
   [response]
   (openai/create-speech {:model "tts-1"
                          :input (read-chat-reply response)
-                         :voice "onyx"
+                         :voice (random-nth ["onyx" "alloy" "nova"])
                          :response_format "mp3"}))
 
 (defn save-stream-to-file
